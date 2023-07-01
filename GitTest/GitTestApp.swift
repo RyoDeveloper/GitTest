30b7f0ec25d828d80ed77830c0774c8e511f9cfe
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-07-01 23:55:29 +0900

    Initial Commit

---
+++ GitTest/GitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitTestApp.swift
+//  GitTest
+//
+//  Created by Ryo on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct GitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
